8e03af56be61270c8c873eeefc3dcfda6699ce06
Joe <josephmvillavicencio@gmail.com>
2021-11-25 11:52:34 -0800

    project complete

--- contracts/MyWaveContract.sol
+++ contracts/MyWaveContract.sol
@@ -4,7 +4,7 @@ import "hardhat/console.sol";
 
 contract MyWaveContract {
     uint256 totalWaves;
-    Wave[] wavers;
+    uint256 private seed;
 
     event NewWave(address indexed from, uint256 timestamp, string message);
 
@@ -14,8 +14,13 @@ contract MyWaveContract {
         uint256 timestamp;
     }
 
-    constructor() {
+    Wave[] wavers;
+
+    mapping(address => uint256) public lastWavedAt;
+
+    constructor() payable {
         console.log("GM");
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function getWavers() public view returns (Wave[] memory) {
@@ -23,9 +28,33 @@ contract MyWaveContract {
     }
 
     function wave(string memory _message) public {
+        require(
+            lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+            "Must wait 30 seconds before waving again."
+        );
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
         wavers.push(Wave(msg.sender, _message, block.timestamp));
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        if (seed <= 50) {
+            console.log("Random # generated: ", seed);
+            console.log("%s won!", msg.sender);
+
+            /*
+             * The same code we had before to send the prize.
+             */
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
         emit NewWave(msg.sender, block.timestamp, _message);
     }
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,27 +1,29 @@
 const main = async () => {
-    const [deployer] = await hre.ethers.getSigners();
-    const accountBalance = await deployer.getBalance();
-  
-    console.log('Deploying contracts with account: ', deployer.address);
-    console.log('Account balance: ', accountBalance.toString());
-  
-    const Token = await hre.ethers.getContractFactory('MyWaveContract');
-    const portal = await Token.deploy();
-    await portal.deployed();
-  
-    console.log('Wave Contract address: ', portal.address);
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.error(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+  const [deployer] = await hre.ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
 
-  // contract addy 0x133Dadddc938A30B47FfADa424d79001F97813e0
+  console.log("Deploying contracts with account: ", deployer.address);
+  console.log("Account balance: ", accountBalance.toString());
+
+  const Token = await hre.ethers.getContractFactory("MyWaveContract");
+  const portal = await Token.deploy({
+    value: hre.ethers.utils.parseEther("0.001"),
+  });
+  await portal.deployed();
+
+  console.log("Wave Contract address: ", portal.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
+
+// contract addy 0x50bc561EAeE5E36CC9D9730142163f46AF736436
